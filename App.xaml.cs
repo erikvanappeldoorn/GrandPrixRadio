936088c7be7b57803877d013d4e72c0e8a105174
Erik van Appeldoorn <erik@nforza.nl>
2022-12-13 13:26:35 +0100

    converted app.xaml

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,54 +1,45 @@
-﻿// Copyright (c) Microsoft Corporation and Contributors.
-// Licensed under the MIT License.
-
-using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
-using Microsoft.UI.Xaml.Shapes;
+﻿using Microsoft.UI.Xaml;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.ApplicationModel;
-using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Windows.Networking.Connectivity;
+using Windows.UI.Popups;
 
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+namespace GrandPrixRadio;
 
-namespace GrandPrixRadio
+public partial class App : Application
 {
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
-    public partial class App : Application
-    {
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
-        public App()
-        {
-            this.InitializeComponent();
-        }
 
-        /// <summary>
-        /// Invoked when the application is launched.
-        /// </summary>
-        /// <param name="args">Details about the launch request and process.</param>
-        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+    private Window m_window;
+
+    public App()
+    {
+        this.InitializeComponent();
+    }
+
+    protected async override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+    {
+        try
         {
+            if (!IsInternetAvailable())
+            {
+                throw new ArgumentException("Internet is not available, application will be closed");
+            }
+
             m_window = new MainWindow();
             m_window.Activate();
             m_window.Title = "Grand Prix Radio";
-        }
 
-        private Window m_window;
+        }
+        catch (Exception ex)
+        {
+            MessageDialog dialog = new MessageDialog(ex.Message);
+            await dialog.ShowAsync();
+            Current.Exit();
+        }
+    }
+
+    public bool IsInternetAvailable()
+    {
+        var connectivityLevel = NetworkInformation.GetInternetConnectionProfile().GetNetworkConnectivityLevel();
+        return connectivityLevel == NetworkConnectivityLevel.InternetAccess;
     }
 }
