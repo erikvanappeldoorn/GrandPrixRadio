297eb55ee42a4ed1624d7256afc84e2b48c4a656
Erik van Appeldoorn <erik@nforza.nl>
2022-12-28 11:08:33 +0100

    correct startup size and icon

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,8 @@ using Microsoft.UI;
 using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using System;
+using System.IO;
+using Windows.ApplicationModel;
 using Windows.Graphics;
 using WinRT.Interop;
 
@@ -17,6 +19,7 @@ public sealed partial class MainWindow : Window
         var windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
         var appWindow = AppWindow.GetFromWindowId(windowId);
 
-        appWindow.Resize(new SizeInt32 { Width = 1280, Height = 1600 });
+        appWindow.Resize(new SizeInt32 { Width = 500, Height = 640 });
+        appWindow.SetIcon(Path.Combine(Package.Current.InstalledLocation.Path, "gpr.ico"));
     }
 }
