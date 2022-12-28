0411b75be7c51b949c934966c23f2ff59ff9e97f
Erik van Appeldoorn <erik@nforza.nl>
2022-12-28 11:29:27 +0100

    corrected height

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,7 +19,7 @@ public sealed partial class MainWindow : Window
         var windowId = Win32Interop.GetWindowIdFromWindow(hWnd);
         var appWindow = AppWindow.GetFromWindowId(windowId);
 
-        appWindow.Resize(new SizeInt32 { Width = 500, Height = 640 });
+        appWindow.Resize(new SizeInt32 { Width = 500, Height = 680 });
         appWindow.SetIcon(Path.Combine(Package.Current.InstalledLocation.Path, "gpr.ico"));
     }
 }
